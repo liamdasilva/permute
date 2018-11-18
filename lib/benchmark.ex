1bd6476e326e4a3f96a52cf77806c32a190d966e
Liam Da Silva <daxs3130@mylaurier.ca>
2018-11-17 22:17:08 -0500

    Add permute method 2

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -1,13 +1,22 @@
 defmodule Benchmark do
+  def run(tests) when is_binary(tests) do
+    run([tests])
+  end
+
   def run(tests \\ ["a", "ab", "abc", "abcd", "abcde", "abcdef"]) do
     Enum.each(tests, fn word ->
       {time, _} = :timer.tc(fn -> Permute.permute(word) end)
-      IO.puts("NON FLOW: Time for #{word}:\t\t\t#{time}")
+      IO.puts("LIAM: Time for #{word}:\t\t\t#{time}")
     end)
 
     Enum.each(tests, fn word ->
-      {time, _} = :timer.tc(fn -> Permute.Flow.permute(word) end)
-      IO.puts("FLOW: Time for #{word}:\t\t\t#{time}")
+      {time, _} = :timer.tc(fn -> Permute2.permute(word) end)
+      IO.puts("ALEX: Time for #{word}:\t\t\t#{time}")
     end)
+
+    # Enum.each(tests, fn word ->
+    #   {time, _} = :timer.tc(fn -> Permute.Flow.permute(word) end)
+    #   IO.puts("LIAM FLOW: Time for #{word}:\t\t\t#{time}")
+    # end)
   end
 end
---
+++ lib/permute2.ex
@@ -0,0 +1,39 @@
+defmodule Permute2 do
+  def permute(word) do
+    permute_aux(String.graphemes(word), [])
+  end
+
+  def permute_aux([], results) do
+    results
+  end
+
+  def permute_aux([head | tail], []) do
+    permute_aux(tail, [head])
+  end
+
+  def permute_aux([head | tail], results) do
+    results = Enum.flat_map(results, &plug_holes(head, &1))
+    permute_aux(tail, results)
+  end
+
+  def plug_holes(letter, string) do
+    plug_holes(letter, string, [], 0, String.length(string))
+  end
+
+  def plug_holes(letter, string, results, index, length) do
+    case index do
+      0 ->
+        results = [letter <> string] ++ results
+        plug_holes(letter, string, results, index + 1, length)
+
+      ^length ->
+        [string <> letter] ++ results
+
+      _ ->
+        front = String.slice(string, 0, index)
+        back = String.slice(string, index, String.length(string))
+        results = [front <> letter <> back] ++ results
+        plug_holes(letter, string, results, index + 1, length)
+    end
+  end
+end
